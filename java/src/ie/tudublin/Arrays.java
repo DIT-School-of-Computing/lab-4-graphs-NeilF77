7be6f160b992c71d9c1cfa45609af191a4529ccc
Neil <c22405604@mytudublin.ie>
2024-02-16 12:16:57 +0000

    First graph finished

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -94,33 +94,30 @@ public class Arrays extends PApplet
 	
 	public void draw()
 	{	
-
 		background(0);
 		float w = width / (float)months.length;
-
+	
 		stroke(255);
-		line(40,40,40,height-40);
-
-		for(int i = 0 ; i < months.length ;  i ++)
+		line(40, 40, 40, height - 60); // Adjust the starting point of the y-axis
+	
+		for(int i = 0; i < months.length; i++)
 		{
-			float x = map1(i, 0, months.length, 0, width);
+			float x = i * w + 40;
 			fill(map1(i, 0, months.length, 0, 255), 255, 255);
-			rect(x, height-30, w, -rainfall[i]);
-
+			rect(x, height - 30, w, -rainfall[i]);
+	
 			textAlign(CENTER, CENTER);
 			fill(255);
-			text(months[i],x+w/2,height - 20);
-			
+			text(months[i], x + w/2, height - 20);
 		}
-
-		line(30, height-30,width,height-30);
-
+	
+		line(30, height - 30, width, height - 30);
+	
 		for (int i = 0; i <= 120; i += 10) {
 			float y = map(i, 0, 120, height - 30, 40);
 			fill(255);
 			text((int) i, 30, y);
 		}
-
 	}
 }
 
